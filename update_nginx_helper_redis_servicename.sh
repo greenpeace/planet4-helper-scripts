994f79cde8e46977b2bab16e107382e0924b9ba6
Raymond Walker <raymond.walker@greenpeace.org>
2018-06-27 10:51:33 +1000

    Usage information.

--- update_nginx_helper_redis_servicename.sh
+++ update_nginx_helper_redis_servicename.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 set -eu
 
+function usage {
+  echo "Usage:
+
+  $(basename "$0") <planet4-release-name> [<redis_servicename>]
+
+Example:
+
+  $(basename "$0") planet4-flibble-release
+
+If <redis_servicename> is not set, the Helm binary is required to detect the
+redis servicename. See https://helm.sh
+
+"
+}
+
+if [[ -z "${1:-}" ]]
+then
+  >&2 echo "Error: Release name not set"
+  >&2 echo
+  usage
+  exit 1
+fi
+
 release=$1
 redis_servicename=${2:-$(helm status $release | grep Service -A 10 | grep redis | head -n1 | cut -d' ' -f1)}
 
