0057d1abc11d49292587c183a98e8abee7397363
Raymond Walker <raymond.walker@greenpeace.org>
2018-06-27 10:51:20 +1000

    Read base64 encoded vars from environment, show usage info, interactive tty test.

--- update_ga_login_secrets.sh
+++ update_ga_login_secrets.sh
@@ -1,14 +1,65 @@
 #!/usr/bin/env bash
 set -eu
 
+function usage {
+  echo "Usage:
+
+  $(basename "$0") <planet4-release-name>
+
+Example:
+
+  $(basename "$0") planet4-flibble-release
+
+The following environment variables are tested for Client ID and Secret:
+
+  GA_CLIENT_ID
+  GA_CLIENT_SECRET
+
+These are expected to be base64 encoded.
+
+If these variables are not set and the terminal is interactive, you can enter
+them at the prompt. If the terminal is not interactive, an error will be shown.
+
+"
+}
+
+if [[ -z "${1:-}" ]]
+then
+  >&2 echo "Error: Release name not set"
+  >&2 echo
+  usage
+  exit 1
+fi
+
 release=$1
 
-read -s -p "Client ID: " client_id
-echo
-read -s -p "Client Secret: " client_secret
-echo
+# Check if interactive
+if ! tty -s
+then
+  # Non-interactive shell, exit with error if variables are unset
+  [[ -z "$GA_CLIENT_ID" ]] && >&2 echo "Error: GA_CLIENT_ID not set." && exit 1
+  [[ -z "$GA_CLIENT_SECRET" ]] && >&2 echo "Error: GA_CLIENT_SECRET not set." && exit 1
+fi
 
-./update_release_wp_array_option.sh $release galogin ga_clientid $client_id
-./update_release_wp_array_option.sh $release galogin ga_clientsecret $client_secret
+if [[ -z "$GA_CLIENT_ID" ]]
+then
+  read -s -p "Google Apps Login client ID: " GA_CLIENT_ID
+  echo
+else
+  # Read base64 encoded var from environment
+  GA_CLIENT_ID=$(base64 -d <<< $GA_CLIENT_ID)
+fi
+
+if [[ -z "$GA_CLIENT_SECRET" ]]
+then
+  read -s -p "Google Apps Login client secret: " GA_CLIENT_SECRET
+  echo
+else
+  # Read base64 encoded var from environment
+  GA_CLIENT_SECRET=$(base64 -d <<< $GA_CLIENT_SECRET)
+fi
+
+./update_release_wp_array_option.sh $release galogin ga_clientid $GA_CLIENT_ID
+./update_release_wp_array_option.sh $release galogin ga_clientsecret $GA_CLIENT_SECRET
 
 ./flush_release_redis.sh $release
