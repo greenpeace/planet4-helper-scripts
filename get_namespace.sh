3629ce65439966934cb72f11c9ad399d88e4e757
Ray Walker <raymond.walker@greenpeace.org>
2018-07-18 11:19:58 +1000

    Fix release var assignment

--- get_namespace.sh
+++ get_namespace.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+release=$1
 
 helm status $release | grep NAMESPACE: | cut -d' ' -f2 | sed 's/planet4-//' | sed 's/-master$//' | sed 's/-release$//' | xargs
