3fd2123d72251233a2de2c64af5fa57e10566cad
Raymond Walker <raymond.walker@greenpeace.org>
2018-08-23 12:24:51 +1000

    Style, shellcheck fixes

--- backup_release_db.sh
+++ backup_release_db.sh
@@ -3,8 +3,8 @@ set -eu
 
 release=$1
 
-namespace=$(./get_namespace.sh $release)
-if ! kubectl get namespace $namespace > /dev/null
+namespace=$(./get_namespace.sh "$release")
+if ! kubectl get namespace "$namespace" > /dev/null
 then
   echo "ERROR: Namespace '$namespace' not found."
   exit 1
@@ -14,14 +14,16 @@ echo "Namespace:  $namespace"
 # Set kubernetes command with namespace
 kc="kubectl -n $namespace"
 
-pod=$($kc get pods -l component=php | grep $release | head -n1 | cut -d' ' -f1)
+pod=$($kc get pods -l component=php | grep "$release" | head -n1 | cut -d' ' -f1)
 echo "Pod:        $pod"
 
 datestring=$(date -u +"%Y%m%dT%H%M%SZ")
 
-$kc exec -ti $pod -- wp db export backup-$datestring.sql
+$kc exec -ti "$pod" -- wp db export "backup-$datestring.sql"
 
-$kc cp $pod:backup-$datestring.sql "$release-$datestring".sql
+mkdir -p backup
+
+$kc cp "$pod:backup-$datestring.sql" "backup/$release-$datestring.sql"
 echo ""
 echo "File downloaded: $release-$datestring.sql"
 echo ""
