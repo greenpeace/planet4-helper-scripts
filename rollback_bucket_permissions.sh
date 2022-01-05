51e9940ecb8137802053047063fc5d672d6d0ca1
Tenorio Cavalcante <tcavalca@greenpeace.org>
2022-01-05 18:58:19 -0400

    added rollback script to update_bucket_permissions.sh

--- rollback_bucket_permissions.sh
+++ rollback_bucket_permissions.sh
@@ -22,7 +22,7 @@ function get_buckets {
   echo ${RESULT[*]}
 }
 
-function set_bucket_level_role {
+function remove_bucket_level_role {
  # this function will apply a role to a bucket
  sa="$1"
  bucket="$2"
@@ -45,43 +45,44 @@ function set_bucket_level_role {
      # is sa memberof roles/storage.admin ?
      if [ "${member_of}" == "\"roles/storage.admin\"" ]
      then
-       echo "=> ${sa} is already member of role ${member_of}"
-       control=0
+       echo "=> ${sa} is a member of role ${member_of}, removing..."
+       # REMOVE IT
+       control=1
        break
      else
-       control=1
+       control=0
      fi
    else
-     control=1
+     control=0
    fi
  done
 
  if [ ${control} -eq 1 ]
  then
    # sa is not a member of roles/storage.admin - adding it
-   echo "adding ${sa} as member of roles/storage.admin"
-   gsutil iam ch serviceAccount:${sa}:roles/storage.admin ${bucket}
+   echo "removing ${sa} membership of roles/storage.admin"
+   gsutil iam ch -d serviceAccount:${sa}:roles/storage.admin ${bucket}
    if [ $? -eq 0 ]
    then
-     echo "=> added ${sa} as member of roles/storage.admin"
+     echo "=> removed ${sa} membership of roles/storage.admin"
    else
-     echo "=> ERROR: could not add ${sa} as member of roles/storage.admin - aborting"
+     echo "=> ERROR: could not remove ${sa} as membership of roles/storage.admin - aborting"
      exit 1
    fi
  fi
 }
 
-function remove_storage_admin_role {
+function set_storage_admin_role {
   sa=$1
   project=$2
   # check if $sa member of roles/storage.admin
   grep -q ${sa} ${STORAGE_ADMIN_MEMBERS}
   if [ $? -eq 0 ]
   then
-    echo "removing ${sa} from roles/storage.admin"
-    gcloud projects remove-iam-policy-binding ${project} --member=serviceAccount:${sa} --role=roles/storage.admin 2>&1
+    echo "=> ${sa} is already a member of ${project} roles/storage.admin"
   else
-    echo "=> $sa is not a ${project} roles/storage.admin member"
+    echo "adding $sa as ${project} roles/storage.admin member"
+    gcloud projects add-iam-policy-binding ${project} --member=serviceAccount:${sa} --role=roles/storage.admin 2>&1
   fi
 }
 
@@ -111,23 +112,20 @@ then
  then
   while IFS= read -r sa
   do
+    # only run set_storage_admin_role if role existsgcloud iam service-accounts list --format json | jq .[].email | grep brazil
+    [ -f ${STORAGE_ADMIN_MEMBERS} ] && set_storage_admin_role ${sa} ${PROJECT}
     echo "Checking buckets for ${sa}"
     sa_buckets=($(get_buckets ${sa}))
     # get the sa_buckets_array lenght
     len=${#sa_buckets[@]}
     if [ ${len} -ge 1 ]
     then
-      # here we can add a fucntion to  check and assign permissions on bucket
-      # also check and remove Storage Admin role
       echo "found buckets for ${sa}:"
       echo ${sa_buckets[*]}
       for i in "${sa_buckets[@]}"
       do
-        echo "applying role to $i"
-        echo "set_bucket_level_role ${sa} $i"
+        remove_bucket_level_role ${sa} $i
       done
-      # only run remove_storage_admin_role if role actually exists
-      [ -f ${STORAGE_ADMIN_MEMBERS} ] && remove_storage_admin_role ${sa} ${PROJECT}
     fi
   done < "${SERVICE_ACCOUNT_LIST}"
   return=0
@@ -138,23 +136,19 @@ then
 else
   # create an array of buckets that belongs to SERVICE_ACCOUNT
   sa=$(cat ${SERVICE_ACCOUNT_LIST} | grep ${SERVICE_ACCOUNT} )
+  # only run set_storage_admin_role if role exists
+  [ -f ${STORAGE_ADMIN_MEMBERS} ] && set_storage_admin_role ${sa} ${PROJECT}
   sa_buckets=($(get_buckets ${sa}))
   len=${#sa_buckets[@]}
   if [ ${len} -ge 1 ]
   then
     echo "found buckets for ${sa}:"
     echo ${sa_buckets[*]}
-    echo "applying new role"
     for i in "${sa_buckets[@]}"
     do
-      echo "applying role to $i"
-      set_bucket_level_role ${sa} $i
+      remove_bucket_level_role ${sa} $i
     done
-  else
-    echo "warning: no buckets found from ${sa}"
   fi
-  # only run remove_storage_admin_role if role actually exists
-  [ -f ${STORAGE_ADMIN_MEMBERS} ] && remove_storage_admin_role ${sa} ${PROJECT}
   return=0
 fi
 
