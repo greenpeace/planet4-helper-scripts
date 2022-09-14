2decd13f9f3f1f6a2353c644a7c4e9c75b7f181d
Foppe Pieters <foppe.pieters@greenpeace.org>
2022-09-14 18:20:34 +0200

    fix: improved multi-gitter scripts

--- multi-gitter/remove_old_cluster_resources.sh
+++ multi-gitter/remove_old_cluster_resources.sh
@@ -3,11 +3,15 @@ set -euo pipefail
 env=$1
 if [ "$env" == "dev" ]; then
 	gcloud container clusters get-credentials p4-development --project planet-4-151612
-	helm ls -n develop --output=json | jq -r '.[].name' | xargs -n 1 echo "helm uninstall -n develop"
-elif [ "$env" == "stage" ]; then
-	gcloud container clusters get-credentials p4-production --zone us-central1-a --project planet4-production
-	helm ls -A --output=json | jq '.[] | select(.chart|test("^wordpress.+")) | select(.name|test("^planet4-.+?-release")) | .namespace, .name' | xargs -n 2 echo "helm delete -n"
-elif [ "$env" == "prod" ]; then
-	gcloud container clusters get-credentials p4-production --zone us-central1-a --project planet4-production
-	helm ls -A --output=json | jq '.[] | select(.chart|test("^wordpress.+")) | select(.name|test("^planet4-.+?-master")) | .namespace, .name' | xargs -n 2 echo "helm delete -n"
+	while read -r resource; do 
+  		helm uninstall -n develop "$resource"
+  		/usr/bin/multi-gitter run --config=config.yml --repo="greenpeace/$resource" ./update_namespace.sh
+	done < <(helm ls -n develop --output=json | jq -r '.[].name')
+elif [ "$env" == "staging" ]; then
+	gcloud container clusters get-credentials planet4-production --zone us-central1-a --project planet4-production
+	while read -r resource; do 
+		helm uninstall -n "$resource"
+		resource2=$(echo "$resource" | cut -d " " -f2 | cut -d "-" -f1,2)
+		/usr/bin/multi-gitter run --config=config.yml --repo=greenpeace/"${resource2}" ./update_namespace.sh
+	done < <(helm ls -a --all-namespaces | grep "release" | awk 'NR > 1 { print  $2, $1}')
 fi
