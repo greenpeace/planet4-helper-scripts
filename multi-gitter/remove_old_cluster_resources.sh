19964ee449b16fce6ee649c78170d7b0495df58e
Foppe Pieters <foppe.pieters@greenpeace.org>
2022-09-15 11:16:54 +0200

    fix: add some comments for easy understanding one year later

--- multi-gitter/remove_old_cluster_resources.sh
+++ multi-gitter/remove_old_cluster_resources.sh
@@ -1,22 +1,56 @@
 #!/bin/bash
 set -euo pipefail
+
+# pick up given environment var (dev/staging/production)
 env=$1
+## development env
 if [ "$env" == "dev" ]; then
+	# get credentials to authenticate
 	gcloud container clusters get-credentials p4-development --project planet-4-151612
+	# read `$resource`
 	while read -r resource; do 
+		# uninstall the helm release
   		helm uninstall -n develop "$resource"
+		# run helm-gitter to update the HELM_NAMESPACE in the git repository
   		/usr/bin/multi-gitter run --config=config.yml --repo="greenpeace/$resource" ./update_namespace.sh
+	# pipe in the helm listing in development namespace and [].name to be used as `resource`
 	done < <(helm ls -n develop --output=json | jq -r '.[].name')
+## staging env
 elif [ "$env" == "staging" ]; then
+	# get credentials to authenticate
 	gcloud container clusters get-credentials planet4-production --zone us-central1-a --project planet4-production
+	# read `$resource`
 	while read -r resource; do 
+		# uninstall the helm release
 		helm uninstall -n <<< echo $resource 
+		# drop the namespace from the `resource` var and cut off -release from the helm release name so we get e.g. "planet4-argentina"
 		resource2=$(echo "$resource" | cut -d " " -f2 | cut -d "-" -f1,2)
+		# run helm-gitter to update the HELM_NAMESPACE in the git repository and push a new tag/release
 		/usr/bin/multi-gitter run --config=config.yml --repo=greenpeace/"${resource2}" ./update_namespace.sh
 		echo "=========="
 		echo " Just finished updating namespaces for ${resource2}"
 		echo " Sleeping for 5 seconds"
 		echo "=========="
 		sleep 5
+	# pipe in the helm listing. grep `release` and pick up the name/namespace and reverse them so we can use them for running `helm uninstall -n` on it
 	done < <(helm ls -a --all-namespaces | grep "release" | awk 'NR > 1 { print  $2, $1}')
+## production env
+elif [ "$env" == "production" ]; then
+	# get credentials to authenticate
+	gcloud container clusters get-credentials planet4-production --zone us-central1-a --project planet4-production
+	# read `$resource`
+	while read -r resource; do 
+		# uninstall the helm release
+		helm uninstall -n <<< echo $resource 
+		# drop the namespace from the `resource` var and cut off -release from the helm release name so we get e.g. "planet4-argentina"
+		resource2=$(echo "$resource" | cut -d " " -f2 | cut -d "-" -f1,2)
+		# run helm-gitter to update the HELM_NAMESPACE in the git repository and push a new tag/release
+		/usr/bin/multi-gitter run --config=config.yml --repo=greenpeace/"${resource2}" ./update_namespace.sh
+		echo "=========="
+		echo " Just finished updating namespaces for ${resource2}"
+		echo " Sleeping for 5 seconds"
+		echo "=========="
+		sleep 5
+	# pipe in the helm listing. grep `release` and pick up the name/namespace and reverse them so we can use them for running `helm uninstall -n` on it
+	done < <(helm ls -a --all-namespaces | grep "master" | awk 'NR > 1 { print  $2, $1}')
 fi
--- multi-gitter/update_namespace.sh
+++ multi-gitter/update_namespace.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
 set -euo pipefail
+
+# create `repo` var without greenpeace/planet-4 prefix
 repo=${REPOSITORY#"greenpeace/planet4-"}
 
+# change to .circleci directory
 pushd .circleci
+
+# update HELM_NAMESPACE in config.yml
+
+## development env 
+## namespace will e.g. be changed from `development` to `argentina`
 #yq -i '.job_environments.develop_environment.HELM_NAMESPACE = "'"${repo}"'"' config.yml
+
+## staging env
+## namespace will e.g. be changed from `argentina-release` to `argentina-staging`
 yq -i '.job_environments.release_environment.HELM_NAMESPACE = "'"${repo}"'-staging"' config.yml
+
+## production env
+## namespace will e.g. be changed from `argentina-release` to `argentina`
 #yq -i '.job_environments.production_environment.HELM_NAMESPACE = "'"${repo}"'"' config.yml
+
+# change back to start directory
 popd
 
+# find current and new version of the tag/releases
 current_version=$(git describe --abbrev=0 --tags)
 new_version=$(perl -pe 's/^(v?(\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e' <<<"$current_version")
+
+echo "=========="
+echo " Tagging new release for ${repo} - ${new_version}"
+echo "=========="
+
+# tag new version and push to circleci to create new production workflow
 git tag -a "$new_version" -m "$new_version"
 git push origin --tags
