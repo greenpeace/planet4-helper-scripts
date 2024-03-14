cbada49af44699e382836b7e93d171553b3b6d62
Foppe Pieters <foppe.pieters@greenpeace.org>
2024-03-14 10:54:40 +0100

    fix: change namespace lookup to kubectl

--- get_namespace.sh
+++ get_namespace.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 release=$1
 
-helm status $release | grep NAMESPACE: | cut -d' ' -f2 | sed 's/planet4-//' | sed 's/-master$//' | sed 's/-release$//' | xargs
+kubectl get pods --all-namespaces -l release=$release -o json | jq -r '.items[].metadata.namespace' | uniq
